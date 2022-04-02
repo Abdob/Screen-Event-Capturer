206cd8a6403bc28275a28c03934540408af457f0
Abdo Babukr <abdo.babukr@wavespectrum.net>
2022-04-01 19:00:47 -0700

    adding recording pipeline

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -2,5 +2,7 @@
 xhost +
 docker run -it --rm --entrypoint="/bin/sh" \
 	-e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v /home/$USER/Videos:/home/Videos \
+	-v /home/$USER/gstreamer_screen_record:/home/gstreamer_screen_record \
 	gst-screen-record
 
