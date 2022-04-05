5415dd6e4533cbed968a6958444b0872e7906355
Abdo Babukr <abdo.babukr@wavespectrum.net>
2022-04-05 13:35:39 -0700

    saving to Video directory

--- install_gstreamer.sh
+++ install_gstreamer.sh
@@ -2,4 +2,4 @@ apt-get update
 apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev
 apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad
 apt-get install -y gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
-
+apt-get install -y gstreamer1.0-vaapi
--- run-screen-record.sh
+++ run-screen-record.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 ximagesrc num-buffers=100 ! videoconvert ! x264enc ! h264parse ! qtmux ! filesink location=video.mp4
+gst-launch-1.0 ximagesrc num-buffers=100 ! videoconvert ! x264enc ! qtmux ! h264parse ! filesink location=/home/Videos/video.mp4
