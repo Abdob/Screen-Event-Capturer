588f323706c7bd6bee51168901874238431c919c
Abdo Babukr <abdo.babukr@wavespectrum.net>
2022-04-05 16:08:59 -0700

    changed resolution

--- screen-record.sh
+++ screen-record.sh
@@ -1,2 +1,2 @@
-gst-launch-1.0 -vvv ximagesrc num-buffers=100 ! videoscale ! video/x-raw,width=640,height=360 ! videoconvert ! x264enc ! h264parse ! mp4mux  ! filesink location=/home/Videos/video.mp4
+gst-launch-1.0 ximagesrc num-buffers=100 ! videoscale ! video/x-raw,width=960,height=540 ! videoconvert ! x264enc ! h264parse ! mp4mux  ! filesink location=video.mp4
 
